288957bf092853de46c640d0a318145dffdcb647
jfarcher <jf_archer@yahoo.com>
2014-12-03 16:04:29 +0000

    Initial commit

---
+++ mqttrepub.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+import mosquitto
+import sys
+import time
+topic = "test"
+sbroker = "192.168.1.1"
+dbroker = "192.168.1.2"
+
+def on_connect(mosq,obj,rc):
+    mosq.subscribe(topic, 0)
+
+def on_message(mosq,obj,msg):
+   message= msg.payload
+   mqtt2 = mosquitto.Mosquitto()
+   mqtt2.connect(dbroker, 1883, 60)
+   mqtt2.publish(topic, message)
+
+mqttc = mosquitto.Mosquitto()
+mqttc.on_connect = on_connect
+mqttc.on_message = on_message
+mqttc.connect(sbroker, 1883, 60)
+
+
+mqttc.loop_forever()
+
